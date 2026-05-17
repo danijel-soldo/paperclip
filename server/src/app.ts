d097e4b4e2e062a5f1008cc77ac16b002cae2e94
d-soldo <d.soldo@ibm.com>
2026-05-17 15:04:51 +0000

    fix(server): comment out broken webhooks import

--- server/src/app.ts
+++ server/src/app.ts
@@ -41,6 +41,7 @@ import { accessRoutes } from "./routes/access.js";
 import { pluginRoutes } from "./routes/plugins.js";
 import { adapterRoutes } from "./routes/adapters.js";
 import { pluginUiStaticRoutes } from "./routes/plugin-ui-static.js";
+// import { router as webhookRoutes } from "./routes/webhooks.js"; // TODO: Fix webhooks implementation
 import { applyUiBranding } from "./ui-branding.js";
 import { logger } from "./middleware/logger.js";
 import { DEFAULT_LOCAL_PLUGIN_DIR, pluginLoader } from "./services/plugin-loader.js";
